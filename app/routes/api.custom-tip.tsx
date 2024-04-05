378a33333855b14853471147eda432195c28b352
jtgi <j@jtgi.me>
2024-04-05 16:10:37 +0900

    send it

--- app/routes/api.custom-tip.tsx
+++ app/routes/api.custom-tip.tsx
@@ -5,67 +5,79 @@ import { neynar } from "~/lib/neynar.server";
 import { getSharedEnv, parseMessage } from "~/lib/utils.server";
 
 export async function action({ request }: ActionFunctionArgs) {
-  const data = await request.json();
-  const env = getSharedEnv();
+  try {
+    const data = await request.json();
+    const env = getSharedEnv();
 
-  const message = await parseMessage(data);
-  const fid = String(message.action.interactor.fid);
-  const hash = message.action.cast.hash;
+    const message = await parseMessage(data);
+    const fid = String(message.action.interactor.fid);
+    const hash = message.action.cast.hash;
 
-  const user = await db.user.findFirst({
-    where: {
-      id: fid,
-    },
-  });
-
-  if (!user || !user.tipAmount || !user.tipType) {
-    return json(
-      {
-        message: `Sign up at ${env.hostUrl}`,
+    const user = await db.user.findFirst({
+      where: {
+        id: fid,
       },
-      { status: 404 }
-    );
-  }
-
-  const allowance = await tipAllowance({ fid: user.id });
-  const tipAmount = computeTipAmount({
-    tipAllowance: allowance,
-    amount: user.tipAmount,
-    type: user.tipType as "pct" | "amt",
-  });
-
-  if (tipAmount === 0) {
-    return json(
-      {
-        message: `No tips left.`,
-      },
-      { status: 400 }
-    );
-  }
-
-  const reply = await neynar
-    .publishCast(user.signerUuid, `${tipAmount} $DEGEN`, {
-      replyTo: hash,
-    })
-    .catch((e) => {
-      console.error(e.message);
-      return null;
     });
 
-  if (reply === null) {
+    if (!user || !user.tipAmount || !user.tipType) {
+      return json(
+        {
+          message: `Sign up at ${env.hostUrl}`,
+        },
+        { status: 404 }
+      );
+    }
+
+    const allowance = await tipAllowance({ fid: user.id });
+    const tipAmount = computeTipAmount({
+      tipAllowance: allowance,
+      amount: user.tipAmount,
+      type: user.tipType as "pct" | "amt",
+    });
+
+    if (tipAmount === 0) {
+      return json(
+        {
+          message: `No tips left.`,
+        },
+        { status: 400 }
+      );
+    }
+
+    const reply = await neynar
+      .publishCast(user.signerUuid, `${tipAmount} $DEGEN`, {
+        replyTo: hash,
+      })
+      .catch((e) => {
+        console.error(e.message);
+        return null;
+      });
+
+    if (reply === null) {
+      return json(
+        {
+          message: `Error tipping. Try again.`,
+        },
+        { status: 400 }
+      );
+    }
+
+    if (reply) {
+      console.log(`${user.id} replied ${reply.hash}`, reply);
+    }
+
+    return json({
+      message: `Tipped ${tipAmount} $DEGEN`,
+    });
+  } catch (e) {
+    console.error(e);
     return json(
       {
-        message: `Error tipping. Try again.`,
+        message: "something went wrong",
       },
-      { status: 400 }
+      {
+        status: 400,
+      }
     );
   }
-
-  if (reply) {
-    console.log(`${user.id} replied ${reply.hash}`, reply);
-  }
-
-  return json({
-    message: `Tipped ${tipAmount} $DEGEN`,
-  });
 }
