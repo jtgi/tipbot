032b23ed4f09c1c31313e5493fc75f21f055494c
jtgi <j@jtgi.me>
2024-04-05 09:05:04 +0900

    instaler

--- app/routes/api.install.tsx
+++ app/routes/api.install.tsx
@@ -2,7 +2,15 @@ import { ActionFunctionArgs } from "@remix-run/node";
 import { redirect } from "remix-typedjson";
 
 export async function action({ request }: ActionFunctionArgs) {
-  return redirect(
-    `https://warpcast.com/~/add-cast-action?actionType=post&name=Tip&icon=gift&postUrl=https%3A%2F%2Fpaperboy.bot%2Fapi%2Fcustom-tip`
-  );
+  const origin = request.headers.get("origin");
+
+  if (origin && origin.includes("nook")) {
+    return redirect(
+      `https://nook.social/~/add-cast-action?actionType=post&name=Tip&icon=gift&postUrl=https%3A%2F%2Fpaperboy.bot%2Fapi%2Fcustom-tip`
+    );
+  } else {
+    return redirect(
+      `https://warpcast.com/~/add-cast-action?actionType=post&name=Tip&icon=gift&postUrl=https%3A%2F%2Fpaperboy.bot%2Fapi%2Fcustom-tip`
+    );
+  }
 }
